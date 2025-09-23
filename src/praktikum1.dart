3a6ae56d1bbbe952257a7c8c44398978f158c97a
Prayogo <riotriprayogo31@gmail.com>
2025-09-23 21:59:17 +0700

    feat: add praktikum 1 langkah 3

--- src/praktikum1.dart
+++ src/praktikum1.dart
@@ -1,11 +1,22 @@
 void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
+  // Langkah 1
+  // var list = [1, 2, 3];
+  // assert(list.length == 3);
+  // assert(list[1] == 2);
+  // print(list.length);
+  // print(list[1]);
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+  // list[1] = 1;
+  // assert(list[1] == 1);
+  // print(list[1]);
+
+  // Langkah 3
+  final List<String?> list = List.filled(5, null);
+
+  list[1] = "Rio Tri Prayogo";
+  list[2] = "2341720236";
+
+  print(list);
+  print("Index 1: ${list[1]}");
+  print("Index 2: ${list[2]}");
 }
