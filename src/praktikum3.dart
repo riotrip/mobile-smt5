6ac37216a844509657d02f01986056ad698740aa
Prayogo <riotriprayogo31@gmail.com>
2025-09-23 22:46:30 +0700

    feat: add praktikum 3 langkah 3

--- src/praktikum3.dart
+++ src/praktikum3.dart
@@ -10,4 +10,17 @@ void main() {
 
   print(gifts);
   print(nobleGases);
+
+  var mhs1 = Map<String, String>();
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+
+  var mhs2 = Map<int, String>();
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+
+  print(gifts);
+  print(nobleGases);
 }
