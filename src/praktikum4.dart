afd4999ffbe19311bb1462b086b4be01b45fd41a
Prayogo <riotriprayogo31@gmail.com>
2025-09-24 13:18:42 +0700

    feat: add praktikum 4 langkah 6

--- src/praktikum4.dart
+++ src/praktikum4.dart
@@ -29,4 +29,9 @@ void main() {
     if (login case 'Manager') 'Inventory',
   ];
   print(nav2);
+
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+  assert(listOfStrings[1] == '#1');
+  print(listOfStrings);
 }
