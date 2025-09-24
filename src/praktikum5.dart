ec5c1418b3e202c411e8222b2e91b758e0214147
Prayogo <riotriprayogo31@gmail.com>
2025-09-24 13:37:44 +0700

    feat: add praktikum 5 langkah 3 tukar

--- src/praktikum5.dart
+++ src/praktikum5.dart
@@ -1,4 +1,11 @@
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
+
 void main() {
   var record = ('first', a: 2, b: true, 'last');
   print(record);
+  
+  print(tukar((1, 2)));
 }
