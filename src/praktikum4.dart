b74805ba645e9530c3e780958481044b73a8fa10
Prayogo <riotriprayogo31@gmail.com>
2025-09-29 00:05:34 +0700

    feat: add some fixation

--- src/praktikum4.dart
+++ src/praktikum4.dart
@@ -7,7 +7,7 @@ void main() {
 
   var list1 = [1, 2, null];
   print(list1);
-  var list3 = [0, ...list1];
+  var list3 = [0, ...?list1];
   print(list3.length);
 
   var bio = ['Rio Tri Prayogo', 2341720236];
