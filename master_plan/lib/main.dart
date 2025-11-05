4f4c577b06956fc53de7f486524e05013075fd21
Prayogo <riotriprayogo31@gmail.com>
2025-11-05 15:34:37 +0700

    feat: add praktikum 1

--- master_plan/lib/main.dart
+++ master_plan/lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import './views/plan_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MasterPlanApp());
 }
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: PlanScreen(),
+    );
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
